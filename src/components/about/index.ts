1671738196b947498d218e920ba773eda64dcf34
Kaushik Varanasi <kaushik.varanasi1@gmail.com>
2021-07-16 19:44:45 +0530

    Add basic server with dockerisation

--- src/components/about/index.ts
+++ src/components/about/index.ts
@@ -1,9 +1,22 @@
-import * as m from 'mithril'
-import nav from '../nav'
-import './index.scss'
+import * as m from 'mithril';
+import nav from '../nav';
+import './index.scss';
+import Pizzly from 'pizzly-js'
+
+const pizzly = new Pizzly({ host: 'https://rocketgraph-first-customers.herokuapp.com' }) // Initialize Pizzly
+const myAPI = pizzly.integration('rocketgraph-first-customers-api-name') // Replace with the API slugname
+
+myAPI
+  .connect()
+  .then(({ authId }) => console.log('Sucessfully connected!', authId))
+  .catch(console.error)
 
 export default {
 	view (vnode) {
+		const email = "";
+		const reddit = "";
+		const twitter = "";
+		const name = "";
         return m("form", [
             m("label.label", "Name (optional)"),
             m("input.input[type=text][placeholder=Name]"),
@@ -15,7 +28,14 @@ export default {
 			m(".tagline", "OR"),
             m("label.label", "Reddit"),
             m("input.input[type=text][placeholder=Reddit]"),
-            m("button.button[type=button]", "Save"),
+            m("button.button[type=button]",{
+				onclick: function() {
+					// call API
+					if(!(email === "" && reddit === "" && twitter === "")) {
+						console.log(name, email, reddit, twitter);
+					}
+				}
+			}, "Save"),
         ])
 	}
 } as m.Component<{}, {}>
